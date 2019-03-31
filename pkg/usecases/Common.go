6567f55bb3a309881fb6e2329f661870d3ad2e04
Sanke <carlosborrero@gmail.com>
2019-03-31 16:55:55 -0300

    CRLF to LF

--- pkg/usecases/Common.go
+++ pkg/usecases/Common.go
@@ -2,6 +2,7 @@ package usecases
 
 import "github.com/SankeProds/jwtservice/pkg/domain"
 
+// repo interface to get users
 type UserRepo interface {
 	FindByName(string) *domain.User
 	Store(*domain.User) bool
